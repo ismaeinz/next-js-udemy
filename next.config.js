b1b5f945fe3b97b270e7d293dc753c02ba827b55
ismaeinz <qismaein@gmail.com>
2023-04-25 13:56:48 +0200

    vercel

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     MONGO_URL:
       "mongodb+srv://ismaeinqasem:ismaein@cluster0.8l7eirz.mongodb.net/products-app",
     APP_DEV: "http://localhost:3000",
-    APP_PROD: "",
+    APP_PROD: "http://next-js-udemy.vercel.app",
   },
 };
 module.exports = nextConfig;
