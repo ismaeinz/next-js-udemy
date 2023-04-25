e3a51a1c7598d4c64160700ed770d58eed1ff216
ismaeinz <qismaein@gmail.com>
2023-04-26 00:23:50 +0200

    finsish

--- next.config.js
+++ next.config.js
@@ -5,11 +5,5 @@ const nextConfig = {
   images: {
     domains: ["images.pexels.com"],
   },
-  env: {
-    MONGO_URL:
-      "mongodb+srv://ismaeinqasem:ismaein@cluster0.8l7eirz.mongodb.net/products-app",
-    APP_DEV: "http://localhost:3000",
-    APP_PROD: "http://next-js-udemy.vercel.app",
-  },
 };
 module.exports = nextConfig;
--- util/mongo.js
+++ util/mongo.js
@@ -1,30 +1,12 @@
-// import mongoose from "mongoose";
-
-// const MONGO_URL = process.env.MONGO_URL;
-// if (!MONGO_URL) {
-//   throw new Error("Invalid environment");
-// }
-// export const dbConnect = async () => {
-//   try {
-//     const { connection } = await mongoose.connect(MONGO_URL);
-//     if (connection.readyState === 1) {
-//       return Promise.resolve(true);
-//     }
-//   } catch (error) {
-//     return Promise.reject(error);
-//   }
-// };
 import mongoose from "mongoose";
 
-// const MONGO_URL = process.env.MONGO_URL;
-// if (!MONGO_URL) {
-//   throw new Error("Invalid environment");
-// }
+const MONGO_URL = process.env.MONGO_URL;
+if (!MONGO_URL) {
+  throw new Error("Invalid environment");
+}
 export const dbConnect = async () => {
   try {
-    const { connection } = await mongoose.connect(
-      "mongodb+srv://ismaeinqasem:ismaein@cluster0.8l7eirz.mongodb.net/products-app"
-    );
+    const { connection } = await mongoose.connect(MONGO_URL);
     if (connection.readyState === 1) {
       return Promise.resolve(true);
     }
